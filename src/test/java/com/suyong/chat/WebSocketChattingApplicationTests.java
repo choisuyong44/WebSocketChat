32793f4a3d93dc0630bd77ec97f09152a3471e6f
choisuyong44 <tlgmdtl1118@inu.ac.kr>
2024-12-06 16:14:44 +0900

    first commit

---
+++ src/test/java/com/suyong/chat/WebSocketChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyong.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
